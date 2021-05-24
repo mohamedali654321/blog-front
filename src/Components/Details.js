78ab766163e73dabe3dc47b75688ca689e3af929
mohamed <mmmmali654321@gmail.com>
2021-05-24 19:51:51 +0000

    first commit

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -2,20 +2,45 @@ import React, { useState, useEffect } from 'react'
 import './Details.css'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import NewsLetterBanner from './NewsLetterBanner';
+import MiniCard from './MiniCard';
 
 function Details({ match }) {
     const slug = match.params.slug;
-    const [details, SetDetails] = useState([])
+    const [details, SetDetails] = useState([]);
+    const [categories, setCategories] = useState();
+    const [cardData, setCardData] = useState([])
     const BACKEND_URL = "http://54.220.211.123:1334";
     const endPoint = `http://54.220.211.123:1334/articles/${slug}`;
+
     useEffect(() => {
         axios.get(endPoint)
             .then(async res => {
 
-                await SetDetails(res.data)
+                await SetDetails(res.data);
+
+                await setCategories(res.data.category.slug)
             })
             .catch(err => console.log(err))
-    }, []);
+    }, [details]);
+
+
+    useEffect(() => {
+        axios.get(`http://54.220.211.123:1334/categories/${categories}`)
+            .then(async res => {
+                await console.log(res.data)
+                await setCardData(res.data);
+
+
+            })
+            .catch(err => console.log(err))
+    }, [categories])
+
+
+
+
+
+
 
 
     return (
@@ -58,14 +83,14 @@ function Details({ match }) {
                     </div>
                     <div className="ContentWrapper">
                         <div className="ContentContainer">
-                            {/* <div className="ShareContainer">
+                            <div className="ShareContainer">
                                 <ul className="share">
                                     <li className="socialMedia">
                                         <Link to="/" className="Button" style={{ color: "#4e6294" }}>
                                             <div className="Icon">
                                                 <i className="fab fa-facebook-f"></i>
                                             </div>
-                                            <span>Facebook</span>
+
                                         </Link>
 
                                     </li>
@@ -75,7 +100,7 @@ function Details({ match }) {
                                             <div className="Icon">
                                                 <i className="fab fa-twitter"></i>
                                             </div>
-                                            <span>Twitter</span>
+
                                         </Link>
                                     </li>
 
@@ -84,31 +109,65 @@ function Details({ match }) {
                                             <div className="Icon">
                                                 <i className="fab fa-linkedin"></i>
                                             </div>
-                                            <span>Linkedin</span>
+
                                         </Link>
                                     </li>
 
                                     <li className="socialMedia">
                                         <Link to="/" className="Button" style={{ color: "#4e6294" }}>
                                             <div className="Icon">
-                                                <i className="fab fa-linkedin"></i>
+                                                <i className="fab fa-youtube"></i>
                                             </div>
-                                            <span>Linkedin</span>
+
+                                        </Link>
+                                    </li>
+                                    <li className="socialMedia">
+                                        <Link to="/" className="Button" style={{ color: "#4e6294" }}>
+                                            <div className="Icon">
+                                                <i className="far fa-envelope"></i>
+                                            </div>
+
                                         </Link>
                                     </li>
 
 
                                 </ul>
 
-                            </div> */}
+                            </div>
                             <div className="InnerContentContainer">
-                               <div className="Content">
-                                   <div className="Author"></div>
-                               </div>
+                                <div className="Content">
+                                    <div className="Author">
+                                        <div className="style-image">
+                                            <div className="ratio">
+                                                <img src="/images/51f6fb256629fc755b8870c801092942.png" className="avatar" />
+                                            </div>
+                                        </div>
+                                        <div >
+                                            <p className="auth">{details.author}</p>
+                                            <p className="date">{details.date}</p>
+                                        </div>
+
+                                    </div>
+                                    <div className="separator"></div>
+                                    <div className="details">
+                                        <blockquote>
+                                            This article is a guest post by <Link to="/" className="nameAuth">{details.author}</Link>
+                                    . He wrote this blog post through the Write for the Community program. If you are passionate about everything jamstack, open-source or javascript .
+                                    </blockquote>
+                                        <div dangerouslySetInnerHTML={{ __html: details.content }}></div>
+
+
+
+
+
+                                    </div>
+                                </div>
 
                             </div>
                         </div>
                     </div>
+
+
 
 
                 </div>
@@ -117,6 +176,49 @@ function Details({ match }) {
 
 
             </section>
+            <div className="RelatedArticlesGrid">
+                <div className="MaxWidth">
+                    <section className="ArticleGradient">
+                        <div className="TitleWrapper">
+                            <div className="InnerTitleWrapper">
+                                <div className="headerWrapper">
+                                    <h1 className="typographyTitle">
+                                        <span className="TitleLabel">You might be interested in</span>
+                                    </h1>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </section>
+
+                    <div className="RelatedArticlesWrapper">
+                        <div className="InnerCardGrid">
+                            <div className="CardGrid">
+                            { cardData.articles &&(
+                                cardData.articles.slice(0,3).map(article=>(
+                                    <MiniCard
+                                        image={BACKEND_URL + article.image.url}
+                                           
+                                           title={article.title}
+                                           text={article.text}
+                                           name={article.author}
+                                           date={article.date}
+                                           slug={article.slug}
+                                    />
+                                ))
+                            )
+                               
+                            }
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <NewsLetterBanner />
 
         </div>
     )
